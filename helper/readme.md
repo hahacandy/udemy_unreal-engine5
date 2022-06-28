7a758e77c0b51fcb6dad6f85118c10b7d4a463ef
hahacandy <58638224+hahacandy@users.noreply.github.com>
2022-06-28 14:12:57 +0900

    약간 변경

--- helper/readme.md
+++ helper/readme.md
@@ -1,3 +1,4 @@
 ![example](https://github.com/hahacandy/udemy_unreal-engine5/blob/main/helper/trans.png)
 
+字幕が変わったら映像をストップして
 英語を日本語に変換してくれる
--- helper/trans.js
+++ helper/trans.js
@@ -2,7 +2,7 @@ function trans(query, el){
 	var result;
 	$.ajax({
 	 type:"POST", //전송방식
-	 url:"https:/your_site/papago.php", //호출 URL
+	 url:"https://thubandiablo.xyz/temp/papago.php", //호출 URL
 	 data:{query:query}, //넘겨줄 데이터
 	 success:function(args){
 	   //alert(args); //통신에 성공했을시 실행되는 함수
@@ -34,13 +34,10 @@ setTimeout(setVideoSubtitles, 1000);
 
 
 var latest_cue_text = '';
-var latest_cue_time = 0;
 var cue_el = null;
 var cue_div = null;
 
 var video = null;
-var sub_changed = true;
-var null_pause = false;
 
 
 function change_subtitles(){
@@ -61,25 +58,15 @@ function change_subtitles(){
 
 		cue_text = cue_el[0].textContent;
 		
-		if (cue_text != null && sub_changed){
-			
-			if(null_pause == true){
-				null_pause = false;
+		if (cue_text != null){
+		
+			if (cue_text != latest_cue_text){
 				latest_cue_text = cue_text;
 				document.getElementById("subtitle").innerHTML = cue_text;
 				trans(cue_text, document.getElementById("subtitle2"));
-				latest_cue_time = video.currentTime;
-			}else if (cue_text != latest_cue_text){
-				sub_changed = false;
 				video.pause();
-				console.log('1');
 			}
-		}
-	}else{
-		if(null_pause == false){
-				null_pause = true;
-				video.pause();
-				console.log('2');
+
 		}
 	}
 	
@@ -92,17 +79,7 @@ function set_video(){
 	if(video == null){
 		setTimeout(set_video, 1000);
 	}else{
-		video.addEventListener('play', (event) => {
-			if(null_pause == false){
-				latest_cue_text = cue_text;
-				document.getElementById("subtitle").innerHTML = cue_text;
-				trans(cue_text, document.getElementById("subtitle2"));
-				latest_cue_time = video.currentTime;
-				sub_changed = true;	
-			}
 
-		});
-		
 		window.addEventListener("keyup", (e) => {
 			console.log(e.keyCode);
 			if(e.keyCode == 32){
@@ -110,9 +87,8 @@ function set_video(){
 					video.play();
 				}
 			}else if (e.keyCode == 38){
-				video.currentTime = latest_cue_time;
+				video.currentTime = latest_cue_time - 0.2;
 				cue_text = latest_cue_text;
-
 				video.play();
 
 				
