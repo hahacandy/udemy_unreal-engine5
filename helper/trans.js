90f680b968a5aa1970a0add49db9e3dd86a8eb49
hahacandy <58638224+hahacandy@users.noreply.github.com>
2022-07-01 09:18:23 +0900

    Update trans.js

--- helper/trans.js
+++ helper/trans.js
@@ -18,37 +18,36 @@ function trans(query, el){
 function setVideoSubtitles(){
 
 	if(document.getElementById('subtitle') == null){
-		
-		document.querySelector('#jp > div.main-content-wrapper > div.main-content > div > div > main').insertAdjacentHTML("beforebegin", "\
+		main_el = document.querySelector('#jp > div.main-content-wrapper > div.main-content > div > div > main');
+		if(main_el==null){
+			main_el = document.querySelector('#udemy > div.main-content-wrapper > div.main-content > div > div > main');
+		}
+		main_el.insertAdjacentHTML("beforebegin", "\
 					<div style='padding:10px;background-color:rgba(0, 0, 0, 0.3);width:100%;text-align:center;text-shadow: black 0px 0px 7px, rgb(0 0 0 / 80%) 0px 0px 18px;color: white;font-size:30px'>\
 					<div id='subtitle'>subtitle</div>\
 					<div id='subtitle2'>subtitle2</div>\
 					</div>");
 		
-	}else{
-		setTimeout(setVideoSubtitles, 1000);
 	}
-	
+	setTimeout(setVideoSubtitles, 1000);
 }
 setTimeout(setVideoSubtitles, 1000);
 
 
 var latest_cue_text = '';
 var cue_el = null;
-var cue_div = null;
 
 var video = null;
 
 
 function change_subtitles(){
 	
-	if (cue_div == null){
-		cue_div_temp = document.getElementsByClassName('captions-display--captions-container--1-aQJ captions-display--user-inactive--2QVjh');
-		if (cue_div_temp.length > 0){
-			cue_div = cue_div_temp;
-			cue_div[0].style.display = 'none';
-		}
+
+	cue_div_temp = document.getElementsByClassName('captions-display--captions-cue-text--ECkJu');
+	if (cue_div_temp.length > 0){
+		cue_div_temp[0].style.display = 'none';
 	}
+
 
 	video = document.getElementsByTagName('video');
 	
